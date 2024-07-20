334a9b38011ccbfd89ed13f218c1c3e33087506d
vikky <tcarts2020to2023@gmail.com>
2024-07-20 20:37:48 +0530

    Updates

---
+++ precache-manifest.fabbc6866f44eaff3d9aaaf33ab0a8e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64f50bb48b155edf0968495344c3d66",
+    "url": "/vikkkyvignesh/React-Todo/index.html"
+  },
+  {
+    "revision": "0d691a6ab509c4a261e9",
+    "url": "/vikkkyvignesh/React-Todo/static/css/main.eadce8b5.chunk.css"
+  },
+  {
+    "revision": "7c5248a3b51110b0e62b",
+    "url": "/vikkkyvignesh/React-Todo/static/js/2.7f41b09f.chunk.js"
+  },
+  {
+    "revision": "c5374957612d9ce17e1ed4ca5659eb87",
+    "url": "/vikkkyvignesh/React-Todo/static/js/2.7f41b09f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d691a6ab509c4a261e9",
+    "url": "/vikkkyvignesh/React-Todo/static/js/main.00f60982.chunk.js"
+  },
+  {
+    "revision": "42f39cbf1748b2242541",
+    "url": "/vikkkyvignesh/React-Todo/static/js/runtime-main.ec769f62.js"
+  },
+  {
+    "revision": "d7a8c2fdbecd8ef9523913e12e77b3a7",
+    "url": "/vikkkyvignesh/React-Todo/static/media/logo.d7a8c2fd.png"
+  },
+  {
+    "revision": "c21e9db68a4145b9c645953eb1fd9859",
+    "url": "/vikkkyvignesh/React-Todo/static/media/neon.c21e9db6.jpg"
+  }
+]);
